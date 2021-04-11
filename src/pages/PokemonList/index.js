5b9791c310510b85c11fd72dec8df5e63fd71d1e
Amalianrlt <amalianurlita97@gmail.com>
2021-04-11 16:35:26 +0700

    Edit Pokemon list page

--- src/pages/PokemonList/index.js
+++ src/pages/PokemonList/index.js
@@ -3,12 +3,22 @@ import { useQuery } from "@apollo/react-hooks";
 import GET_POKEMONS from "../../graphql/getAllPokemons";
 import { PokemonCard } from "../../templates";
 
+var Color = [
+  "#BEEFE4",
+  "#C9C9EF",
+  "#FFB0AC",
+  "#FFEE8E",
+  "#C7E5A2",
+  "#FBD4B6",
+  "#E6E6F1",
+];
+
 const PokemonList = () => {
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
     variables: { first: 9 },
   });
 
-  console.log(pokemons);
+  // console.log(pokemons);
   return (
     <div
       style={{
@@ -24,12 +34,15 @@ const PokemonList = () => {
         justifyContent: "space-between",
       }}
     >
-      <PokemonCard width={"23%"} />
-      <PokemonCard width={"23%"} />
-      <PokemonCard width={"23%"} />
-      <PokemonCard width={"23%"} />
-      <PokemonCard width={"23%"} />
-      <PokemonCard width={"23%"} />
+      {pokemons &&
+        pokemons?.results?.map((pokemon, idx) => (
+          <PokemonCard
+            width={"23%"}
+            data={pokemon}
+            key={idx}
+            bgColor={Color.map((c) => c)}
+          />
+        ))}
     </div>
   );
 };
